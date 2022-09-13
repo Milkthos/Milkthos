95bf1acdf526bc0f0293e383fc73e7151e56f7d2
Milkthos <jenhuilau@gmail.com>
2022-09-13 16:56:25 +1000

    New icons, rick roll but good

--- www/src/main.ts
+++ www/src/main.ts
@@ -1,3 +1,10 @@
-console.log("Hello World!");
+document.addEventListener("DOMContentLoaded", () => {
+    let rickButton = document.getElementById("rick");
+
+    rickButton?.addEventListener("mouseenter", () => {
+        console.log("oo a mouse");
+        document.body.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/iJgNpm8cTE8?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    });
+});
 
 export {};
